70c264ee5eb5600f1e96ca8c9f960e61a284ee34
Vincent_Liussyaputra <liusvinv@gmail.com>
2023-08-02 12:52:13 +0700

    Remove caching

--- src/app/db/sanityUtils.ts
+++ src/app/db/sanityUtils.ts
@@ -4,7 +4,7 @@ import imageUrlBuilder from '@sanity/image-url'
 export const sanityClient = createClient({
 	projectId:'6kn1yqrj',
 	dataset:'local',
-	useCdn:true,
+	useCdn:false,
 	apiVersion:'2023-05-03'
 })
 
