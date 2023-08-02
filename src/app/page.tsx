ff5e04ea97dff95c0defc48fa4aa9d5030944b00
Vincent_Liussyaputra <liusvinv@gmail.com>
2023-08-02 13:10:03 +0700

    Fix caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import TitleHome from "./components/home/TitleHome"
 import Profile from "./home/Profile"
 
+export const revalidate = 10
+
 export default function Home() {
   return (
     <div className="page">
