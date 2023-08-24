9f121accd2b3e16698fdf51af8813678853f898b
Vincent_Liussyaputra <liusvinv@gmail.com>
2023-08-25 06:08:19 +0700

    Optimize spell decor

--- src/app/components/general/decor/SpellDecor.tsx
+++ src/app/components/general/decor/SpellDecor.tsx
@@ -8,8 +8,9 @@ const SpellDecor = () => {
 	const sidebarContext = useContext(SidebarCollapseContext)
 	return (
 			<div className={`decor_spell ${sidebarContext.state ? '' : 'hidden'} `}>
-				<img src="/static/images/decor/magic_circle_animated.gif" alt="" className='top-spell_l' width={300} height={300}/>
-				<img src="/static/images/decor/magic_circle_animated.gif" alt="" className='top-spell_r' width={300} height={300}/>
+				<video autoPlay loop src="/static/images/decor/magic_circle_animated.mp4" className='top-spell_l' width={300} height={300}/>
+				<video autoPlay loop src="/static/images/decor/magic_circle_animated.mp4"  className='top-spell_r' width={300} height={300}/>
+				{/* <img src="/static/images/decor/magic_circle_animated.gif" alt="" className='top-spell_r' width={300} height={300}/> */}
 			</div>
 	)
 }
